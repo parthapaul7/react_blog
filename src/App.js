2c73997ff8beb187f353223a218ef802bc3450a2
partha <pparthapaul11@gmail.com>
2021-08-05 22:24:02 +0530

    delete method corrected

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Write from "./Write";
 import Allposts from "./Allposts";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
+import { Button,Modal } from "react-bootstrap";
 
 export default function Posts({ element, del, value, count }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+ 
+  const  [tit, settit] = useState("default");
+  const [desc, setdesc] = useState("default")
+  function pass(a,b){
+    settit(a);
+    setdesc(b);
+
+  console.log(tit,desc);
+  
+
+  }
 
 
 
@@ -8,63 +24,29 @@ export default function Posts({ element, del, value, count }) {
     <div className="container my-3">
       <div className="card">
         <div className="card-header">
-          <p>SL no. {count}</p> <p> Posted at --- {JSON.parse(element).date}</p>
+          <p>SL no.  {count}</p> <p> Posted at --- {JSON.parse(element).date}</p>
         </div>
         <div className="card-body">
           <h5 className="card-title">{JSON.parse(element).title}</h5>
           <p className="card-text">{JSON.parse(element).descrip}</p>
-          <button
-            type="button"
-            className="btn btn-danger"
-            data-toggle="modal"
-            data-target="#exampleModalCenter"
-          >
-            Delete Post
-          </button>
+          
 
-          <div
-            className="modal fade"
-            id="exampleModalCenter"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalCenterTitle"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog modal-dialog-centered" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLongTitle"></h5>
-                </div>
-                <div className="modal-body">
-            
-                  Your post Will be deleted Permanently
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-danger"
-                    data-dismiss="modal"
-                    onClick={() => {
-                      del(
-                        JSON.parse(element).title,
-                        JSON.parse(element).descrip
-                      );
-                      value();
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Button variant="danger" onClick={()=>{handleShow();pass(JSON.parse(element).title,JSON.parse(element).descrip)}}>
+        Delete Post
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+       
+        <Modal.Body>Your Post : "{tit}" will be permanently deleted</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={()=>{handleClose(); del(tit,desc);value()}}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
         </div>
       </div>
     </div>
